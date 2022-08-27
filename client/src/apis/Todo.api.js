da8e9ed617796d3e4f418129f4c6e0926e806f50
DominykMcNamara <dominyksmith@gmail.com>
2022-08-27 19:42:21 -0400

    update

--- client/src/apis/Todo.api.js
+++ client/src/apis/Todo.api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://toasty-brioche.herokuapp.com/api/v1/todos"
+    baseURL: "https://toasty-brioche.herokuapp.com"
 })
